b2aa36b718ab2a65402e30268ee315a017f2ee7e
pac-nam <thomasbleuse02@gmail.com>
2020-07-03 17:16:37 +0200

    no test done on heuristic manhattan

--- solver/Solve.go
+++ solver/Solve.go
@@ -46,3 +46,61 @@ func heuristicPlacement(ctx *n.SContext) int {
 
 	return (score)
 }
+
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func littleManhattan(nb, Ystart, Xstart, NSize int) int {
+	X, Xmin, Xmax, Y, Ymin, Ymax := 0, 0, NSize-1, 0, 1, NSize-1
+	square := NSize * NSize
+	for i := 1; i < square; {
+		for X < Xmax {
+			if nb == i {
+				return abs(X-Xstart) + abs(Y-Ystart)
+			}
+			i++
+			X++
+		}
+		Xmax--
+		for Y < Ymax {
+			if nb == i {
+				return abs(X-Xstart) + abs(Y-Ystart)
+			}
+			i++
+			Y++
+		}
+		Ymax--
+		for X > Xmin {
+			if nb == i {
+				return abs(X-Xstart) + abs(Y-Ystart)
+			}
+			i++
+			X--
+		}
+		Xmin++
+		for Y > Ymin {
+			if nb == i {
+				return abs(X-Xstart) + abs(Y-Ystart)
+			}
+			i++
+			Y-- 
+		}
+		Ymin++
+	}
+	return 0
+}
+
+func manhattanHeuristic(ctx *n.SContext) int {
+	score := 0
+	for Y, line := range ctx.Puzzle {
+		for X, nb := range line {
+			score += littleManhattan(nb, Y, X, ctx.NSize)
+		}
+	}
+	return score
+}
