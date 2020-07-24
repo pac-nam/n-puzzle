4f07c72c8990713ddd123616c946fefc21ed004b
user42 <ayguillo@student.42.fr>
2020-07-24 16:49:13 +0200

    [ADD] SImages

--- structures/SImage.go
+++ structures/SImage.go
@@ -5,17 +5,12 @@ import (
 )
 
 type SImage struct {
-	Heuristic int
 	Cost      int
-	NSWE      uint8
-	Zero      SVertex     // coordinates of zero square
 	Puzzle    [][]Tnumber // puzzle grid
-	Path      []Tnumber
 }
 
 func (node SImage) String() string {
 	res := "---------------SImage---------------\n"
-	res += "Heuristic : " + fmt.Sprint(node.Heuristic)
 	res += "Cost : " + fmt.Sprint(node.Cost) + "\n"
 	// res += "Size : " + fmt.Sprint(node.Ctx.NSize) + "\n"
 	// res += "Final : " + fmt.Sprint(node.Ctx.Final) + "\n"
@@ -28,7 +23,6 @@ func (node SImage) String() string {
 		}
 		res += "\n"
 	}
-	res += "}\nZero : " + fmt.Sprint(node.Zero) + "\n"
-	res += "Path: " + fmt.Sprint(node.Path)
+	res += "}"
 	return res
 }
